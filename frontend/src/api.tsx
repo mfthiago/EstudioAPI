cbdb42f4f87247549565e8bbcd65db165b2a86c6
Thiago Mello <mfthiagod@gmail.com>
2024-07-10 14:32:16 -0300

    Add

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -7,8 +7,8 @@ interface SearchResponse{
 
 export const searchCompanies = async (query:string) => {
     try{
-        const data = await axios.get<SearchResponse>(`http://localhost:5000/search/${process.env.REACT_APP_API_KEY}/${query}`);
-        return data;
+        const data = await axios.get<SearchResponse>("https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}");
+          return data;
         
     } catch (error){
         if(axios.isAxiosError(error)){
